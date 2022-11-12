c7bcaaf1dce79ea9ea5181e411d9baf05d24f53b
Adam <adamdiallo2k@gmail.com>
2022-11-12 09:50:39 -0800

    %d and %i

--- _printf.c
+++ _printf.c
@@ -3,6 +3,47 @@
 #include <stdlib.h>
 #include "main.h"
 #include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * print_int - Entry point
+ * Description: 'return difference'
+ * Return: int
+ * @c: va_list parameter
+ */
+int print_int(va_list c)
+{
+	int rm10;
+	int dvsr;
+	int quot;
+	int count = 0;
+	int r = va_arg(c, int);
+
+	if (r < 0)
+	{
+		write(1, "-", 1);
+		count++;
+	}
+	r = abs(r);
+	rm10 = r / 10;
+	dvsr = 1;
+
+	while (dvsr <= rm10)
+		dvsr *= 10;
+
+	while (dvsr)
+	{
+		quot = r / dvsr;
+		quot = quot + '0';
+		write(1, &quot, 1);
+		r %= dvsr;
+		dvsr /= 10;
+		count++;
+	}
+	return (count);
+}
+
+
 
 /**
  * print_chare - Entry point
@@ -54,7 +95,8 @@ int _printf(const char * const format, ...)
 {
 	int y, i, lenght = 0;
 	va_list parametersInfos;
-	st_t stu[] = {{'c', print_char}, {'s', print_chare}, {0, NULL}};
+	st_t stu[] = {{'c', print_char}, {'s', print_chare}, {'d', print_int}
+		, {'i', print_int}, {0, NULL}};
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
